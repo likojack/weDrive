b0201bde1279528666d2165f39d939805e415fa9
JeanetteZ <jeanette.t.zhang@gmail.com>
2015-08-22 22:02:32 +1000

    app delegate edited to include facebook stuff

--- weDrive/AppDelegate.swift
+++ weDrive/AppDelegate.swift
@@ -45,6 +45,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+		FBSDKAppEvents.activateApp()
     }
 
     func applicationWillTerminate(application: UIApplication) {
@@ -54,7 +55,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     // MARK: - Core Data stack
-
+	func application(application: UIApplication,
+		openURL url: NSURL,
+		sourceApplication: String?,
+		annotation: AnyObject?) -> Bool {
+			return FBSDKApplicationDelegate.sharedInstance().application(application, openURL: url, sourceApplication: sourceApplication, annotation: annotation)
+	}
+	
     lazy var applicationDocumentsDirectory: NSURL = {
         // The directory the application uses to store the Core Data store file. This code uses a directory named "com.ShumanLiu.weDrive" in the application's documents Application Support directory.
         let urls = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
