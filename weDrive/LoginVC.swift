eea3218d132748eccf3c840d224c300720cc922d
JeanetteZ <jeanette.t.zhang@gmail.com>
2015-08-22 21:25:11 +1000

    added login and signup VC swift files locally

---
+++ weDrive/LoginVC.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  WeDriveTong
+//
+//  Created by Jeanette Zhang on 19/07/2015.
+//  Copyright (c) 2015 Jeanette Zhang. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginVC: UIViewController {
+	/*********** Basic login ***********/
+	@IBOutlet weak var loginEmailField: UITextField!
+	@IBOutlet weak var loginPasswordField: UITextField!
+	
+	//Login Button	
+	@IBAction func loginButton(sender: AnyObject) {
+		var loginEmail = self.loginEmailField.text
+		var loginPassword = self.loginPasswordField.text
+		
+		
+		//TODO: verify email and password
+		if (loginEmail.isEmpty || loginPassword.isEmpty){
+			var incompleteLoginInfoAlert = UIAlertView(title: "Invalid", message: "Please enter your email address and password", delegate: self, cancelButtonTitle: "OK")
+			incompleteLoginInfoAlert.show()
+		} else { //email and password verified
+			PFUser.logInWithUsernameInBackground(loginEmail, password: loginPassword, block: {(user, error) -> Void in
+			//if inside this block, request was successful
+				if ((user) != nil){
+					println("User logged in through Parse \(user)")
+					self.performSegueWithIdentifier("loginSegue", sender: self)
+				} else {
+					println("User failed to login")
+				}
+			})
+		}
+	}
+	
+	//Signup Button
+	@IBAction func signupButton(sender: AnyObject) { self.performSegueWithIdentifier("signupSegue", sender: self)
+	}
+	
+	
+	/*********** Login with Facebook ***********/
+	let permissions = ["public_profile", "email", "user_friends"]
+
+	@IBAction func faceBookLoginButton(sender: UIButton) {
+		PFFacebookUtils.logInInBackgroundWithReadPermissions(permissions, block: {(user:PFUser?, error: NSError?) -> Void in
+			if user == nil { NSLog("User cancelled the Facebook login.") }
+			else if user!.isNew { NSLog("User signed up and logged in through Facebook! \(user)") }
+			else if user != nil{NSLog("User logged in through Facebook! \(user)") }
+		})
+	}
+	
+
+
+
+}
+
---
+++ weDrive/SignupVC.swift
@@ -0,0 +1,48 @@
+//
+//  SignupVC.swift
+//  WeDriveTong
+//
+//  Created by Jeanette Zhang on 19/08/2015.
+//  Copyright (c) 2015 Jeanette Zhang. All rights reserved.
+//
+
+import UIKit
+import Parse
+class SignupVC: UIViewController{
+	
+	@IBOutlet weak var signupFirstnameField: UITextField!
+	
+	@IBOutlet weak var signupLastnameField: UITextField!
+	
+	@IBOutlet weak var signupEmailField: UITextField!
+	
+	@IBOutlet weak var signupPasswordField: UITextField!
+	
+	@IBOutlet weak var signupRepeatPasswordField: UITextField!
+	//TODO: check repeat password is same as password
+	
+	@IBAction func creationAccountButton(sender: AnyObject) {
+		var signupFirstname = signupFirstnameField.text
+		var signupLastname = signupLastnameField.text
+		
+		var signupEmail = signupEmailField.text
+		var signupPassword = signupPasswordField.text
+		
+		var newUser=PFUser()
+		newUser.username = signupEmail //NOTE: use email as username
+		newUser.password = signupPassword
+		newUser.email = signupEmail
+		newUser.signUpInBackgroundWithBlock({ (succeed, error) -> Void in
+			if(error != nil){
+				var incompleteSignupInfoAlert = UIAlertView(title: "Invalid", message: "Please complete all fields", delegate: self, cancelButtonTitle: "OK")
+				incompleteSignupInfoAlert.show()
+			} else { //user created
+				println("User successfully signed up through Parse! \(newUser)")
+				self.performSegueWithIdentifier("SignupLoginSegue", sender: self)
+			}
+		})
+		
+
+	}
+	
+}
